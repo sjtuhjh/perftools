48a140f701dcd201afeab0e72d6f7a7a91558670
root <root@CentOS.HOST>
2016-10-13 01:51:03 +0000

    update kernel build scripts

--- utils/build_kernel.sh
+++ utils/build_kernel.sh
@@ -15,17 +15,17 @@ fi
 pushd ${KERNEL_SRC} > /dev/null
 
 ./scripts/kconfig/merge_config.sh -m arch/arm64/configs/defconfig arch/arm64/configs/distro.config arch/arm64/configs/estuary_defconfig
-make ARCH=arm64 menuconfig
+#make ARCH=arm64 menuconfig
 mv -f .config .merged.config
 make ARCH=arm64 ${CROSS_CMD} KCONFIG_ALLCONFIG=.merged.config alldefconfig
 make ARCH=arm64 ${CROSS_CMD} Image -j40
 
 make ARCH=arm64 ${CROSS_CMD} modules
 
-if [ "$(uname -m)" == "aarch64" ] ; then
+#if [ "$(uname -m)" == "aarch64" ] ; then
 #make ARCH=arm64 ${CROSS_CMD} modules_install
 #make ARCH=arm64 install
-fi
+#fi
 
 popd > /dev/null
 
