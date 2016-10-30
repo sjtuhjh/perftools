773c0011b6ceb8eaf218b9367d25b33a6550c769
root <root@huangjinhua>
2016-10-30 22:28:02 +0800

    update cmake build script

--- bcc/setup.sh
+++ bcc/setup.sh
@@ -5,15 +5,11 @@ if [ "$(whoami)" == "root" ] ; then
     SUDO_PREFIX=""
 fi
 
-
-
-
-
 INSTALL_CMD="yum install"
-PACKAGES="subversion bison gcc gcc-c++ make cmake-3* flex git libedit-devel mesa-private-llvm mesa-private-llvm-devel python zlib-devel elfutils-libelf lua lua-devel"
+PACKAGES="subversion bison gcc gcc-c++ make cmake-3* flex git clang libedit-devel mesa-private-llvm mesa-private-llvm-devel python zlib-devel elfutils-libelf lua lua-devel"
 if [ "$(which apt-get 2> /dev/null)" ] ; then
     INSTALL_CMD="apt-get install"
-    PACKAGES="subversion bison build-essential cmake_3* flex git libedit-dev libllvm3.7 llvm-3.7-dev libclang-3.7-dev python zlib1g-dev libelf-dev luajit luajit-5.1-dev"
+    PACKAGES="subversion bison build-essential cmake-3.7* flex git clang libedit-dev libllvm3.7 llvm-3.7-dev libclang-3.7-dev python zlib1g-dev libelf-dev luajit luajit-5.1-dev"
 else
     ${SUDO_PREFIX} yum install -y "Development Tools"
 fi
@@ -23,22 +19,23 @@ build_and_install_clang() {
     #Download and install clang 
     mkdir builddir
     pushd builddir > /dev/null
-
+    
     if [ ! -f "cmake-3.7.0-rc1.tar.gz" ]; then   
         wget https://cmake.org/files/v3.7/cmake-3.7.0-rc1.tar.gz
         tar -zxf cmake-*.tar.gz
     fi
 
     cd cmake-3.7.0-rc1  
-    ./bootstrap && make && make install
+    ./bootstrap --parallel=32
+    make && make install
     cd ..
 
-    svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+    svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_370/final/ llvm
     cd llvm
     svn update
 
     cd tools
-    svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+    svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_370/final/ clang
     cd clang
     svn update
 
@@ -74,7 +71,7 @@ build_and_install_clang() {
 
 ${SUDO_PREFIX} ${INSTALL_CMD} -y  ${PACKAGES}
 
-if [ -z "$(which clang)" ] ; then
+if [ ! -z "$(which clang)" ] ; then
     echo "Begin to download and install clang......."
     build_and_install_clang 
 fi
