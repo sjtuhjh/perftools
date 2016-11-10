6e9de175ae92ee129f7fc1cb55310806c877bedd
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2016-11-10 19:34:59 +0000

    add performance schema tool

---
+++ apptools/mysql/mysql_sys_update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/mysql/mysql-sys.git
+
--- apptools/mysql/show_processlist.sh
+++ apptools/mysql/show_processlist.sh
@@ -6,8 +6,15 @@ else
     MYSQL="mysql"
 fi
 
-PROCESS_LOG="processlist-`date +%F-%H:%M`.log"
+#PROCESS_LOG="processlist-`date +%F-%H:%M`.log"
 
-${MYSQL} -uroot -p -e "show full processlist" > ./${PROCESS_LOG}
+PROCESS_LOG="tmp_log"
+
+while [[ 1 ]] 
+do
+${MYSQL} -uroot -p --socket=/u01/u0/my3306/run/mysql.sock -e "show full processlist" > ./${PROCESS_LOG}
 grep State: ./${PROCESS_LOG} | sort | uniq -c | sort -rn
+sleep
+
+done
 
