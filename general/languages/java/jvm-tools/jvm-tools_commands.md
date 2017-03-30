5159c57fcdc6b0177c471f5b7a1573609aea4afd
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-03-30 11:38:05 +0800

    add jvm tools

---
+++ general/languages/java/jvm-tools/jvm-tools_commands.md
@@ -0,0 +1,3 @@
+> As for detailed commands, please refer to [Manual](https://github.com/aragozin/jvm-tools/blob/master/jsk-core/COMMANDS.md)
+>
+> 
---
+++ general/languages/java/jvm-tools/jvm-tools_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/aragozin/jvm-tools
+
+wget https://bintray.com/artifact/download/aragozin/generic/sjk-0.5.1.jar
