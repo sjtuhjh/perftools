be914d47ca471eca9768c7a34b82616ec9fe50c6
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-05-16 10:21:45 +0800

    update sysdig scripts

--- general/common/sysdig_setup.sh
+++ general/common/sysdig_setup.sh
@@ -3,6 +3,10 @@
 git clone https://github.com/sjtuhjh/sysdig
 
 cd sysdig
+git branch master
+git checkout master
+git branch --set-upstream-to=origin/master master
+git pull
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=release ..
